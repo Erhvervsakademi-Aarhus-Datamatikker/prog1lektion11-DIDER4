ce9fd3f829cd0265c0890d637f03d18ebba51d5a
DIDER <Simon.dider@gmail.com>
2024-09-25 19:46:50 +0200

    opgave

--- src/opgave05/Showstock.java
+++ src/opgave05/Showstock.java
@@ -1,4 +1,8 @@
 package opgave05;
 
 public class Showstock {
+    public static void main(String[] args) {
+        stock stock1 = new stock("ORCL","Oracle Corporation", 34.5, 34.35);
+        System.out.println(stock1.getChangePercent());
+    }
 }
--- src/opgave05/stock.java
+++ src/opgave05/stock.java
@@ -1,4 +1,24 @@
 package opgave05;
 
 public class stock {
+    String symbol;
+    String name;
+    double previousClosingPrice;
+    double currentPrice;
+
+    stock(){
+    }
+
+    stock(String newSymbol, String newName, double newPreviousClosingPrice, double newCurrentPrice){
+        symbol = newSymbol;
+        name = newName;
+        previousClosingPrice = newPreviousClosingPrice;
+        currentPrice = newCurrentPrice;
+    }
+
+    double getChangePercent(){
+        return previousClosingPrice / currentPrice * 100;
+    }
+
 }
+
